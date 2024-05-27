4e29500a802d558d750f4086fa1639fb267549e7
Maciej <maciekjur123@gmail.com>
2024-05-28 00:21:14 +0200

    api key działa

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 
 export default ({ config }) => {
+   console.log("google maps api key nasze:" + process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY)
     return {
         ...config,
         ios: {
             ...config.ios,
             config: {
-                googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+                googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
             },
             infoPlist: {
                 NSLocationWhenInUseUsageDescription: "This app uses your location to show your position on the map."
@@ -16,12 +17,15 @@ export default ({ config }) => {
             ...config.android,
             config: {
                 googleMaps: {
-                    apiKey: process.env.GOOGLE_MAPS_API_KEY
+                    apiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY
                 }
             }
         },
         extra: {
-            googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+            googleMapsApiKey: process.env.EXPO_PUBLIC_GOOGLE_MAPS_API_KEY,
+            eas: {
+                projectId: "134f1c30-c6b1-47ff-a0db-e4cb41ef9255"
+            }
         }
     };
 };
